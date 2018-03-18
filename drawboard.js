760a392643f131ad9379421e5c3b6f03c39b7ce0
Tatevik Shahnazaryan <tatev19@gmail.com>
2018-03-18 22:29:35 +0400

    Input fields

--- drawboard.js
+++ drawboard.js
@@ -1,16 +1,104 @@
 window.onload = function() {
-    
     let array = JSON.parse(localStorage.getItem('checked'));
-   
-    
     checkColumns(array);
-   
+
 }
+
+function createFields() {
+    let editBtn  = document.getElementsByClassName('edit');
+    let member = document.getElementsByClassName('member');
+    editBtn = Array.from(editBtn);
+    member = Array.from(member);
+    
+
+    for (let i = 0; i < editBtn.length; i++) {
+    editBtn[i].addEventListener('click', inputFields);
+    function inputFields() {
+        let mainRow = document.getElementById('mainRow');
+        let div = document.createElement('div');
+        let span = document.createElement('span');
+        let divContent = document.createElement('div');
+        let inputName = document.createElement('input');
+        let inputSurname = document.createElement('input');
+        let inputSolvedTasks = document.createElement('input');
+        let inputReadPages = document.createElement('input');
+        let inputSpentTime = document.createElement('input');
+        let button = document.createElement('button');
+        let inputArray = [inputName, inputSurname, inputSolvedTasks, inputReadPages, inputSpentTime];
+        
+        inputName.className = "modalName";
+        inputSurname.className = "modalSurname";
+        inputSolvedTasks.className = "modalSolvedTasks";
+        inputReadPages.className = "modalReadPages";
+        inputSpentTime.className = "modalSpentTime";
+        button.className = "modalSave";
+        button.innerText = "Save";
+        inputName.placeholder = "Member Name";
+        inputSurname.placeholder = "Member Surname";
+        inputSolvedTasks.placeholder = "Member SolvedTasks";
+        inputReadPages.placeholder = "Member ReadPages";
+        inputSpentTime.placeholder = "Member SpentTime";
+    
+        for (let j = 0; j < inputArray.length; j++) {
+            inputArray[j].classList.add('input');
+            divContent.appendChild(inputArray[j]);
+        }
+        divContent.appendChild(button);
+        span.className = 'close';
+        span.innerHTML = '&times;'
+        div.className = 'modal';
+        divContent.className = 'modal-content';
+        
+       
+        div.appendChild(divContent);
+        div.appendChild(span);
+        let nodes = member[i].childNodes;
+        nodes = Array.from(nodes);
+        nodes.map(function(item) {
+            item.style.display = 'none';
+        })
+        member[i].appendChild(div);
+    
+            
+            button.addEventListener('click', function() {
+                div.style.display='none';
+                Array.from(member[i].childNodes).map(function(item) {
+                     if (item.className !== 'modal') {
+                        document.getElementsByClassName("name")[i].innerText = inputName.value;
+                        document.getElementsByClassName("surname")[i].innerText = inputSurname.value;
+                        document.getElementsByClassName("problems")[i].innerText = inputSolvedTasks.value;
+                        document.getElementsByClassName("pages")[i].innerText = inputReadPages.value;
+                        //document.getElementsByClassName("spent-time")[i].innerText = inputSpentTime.value;
+                        item.style.display = 'flex';
+                     } else {
+                        item.style.display = 'none';
+                     }
+                     
+                 })
+                 
+             })
+    
+            span.addEventListener('click', function() {
+                div.style.display='none';
+                Array.from(member[i].childNodes).map(function(item) {
+                     if (item.className !== 'modal') {
+                        item.style.display = 'flex';
+                     } else {
+                        item.style.display = 'none';
+                     }
+                 })
+             })
+        }
+
+    }  
+}
+
 
 
 function addRow(memberinput, currentRow) {
        
         if (memberinput < 1) {
+            createFields();
             return;
         }
 
@@ -22,7 +110,8 @@ function addRow(memberinput, currentRow) {
         
         document.getElementById('background').appendChild(div);
         return addRow(memberinput - 1, currentRow);
-    }
+    
+}
 
     
 
@@ -32,8 +121,8 @@ function addRow(memberinput, currentRow) {
 
 
 function checkColumns(array) {
-    let currentRow = "<span id='name'></span><span id='score'></span>";
-    let row = "<span>Name Surname</span><span>Score</span>";
+    let currentRow = "<span class='name'></span><span class='surname'></span><span class='score'></span>";
+    let row = "<span>Name</span><span>Surname</span><span>Score</span>";
 
     array.map(function(item) {
         if (item === 0) {
@@ -68,18 +157,10 @@ function addFirstLine(row, currentRow) {
             row;
     
         addRow(number, currentRow);
-        
+        let newMember = document.getElementsByClassName("addButton")[0];
+        newMember.onclick = function() {addRow(1, currentRow)};
         
         
 }
-let newMember = document.getElementsByClassName("addButton")[0];
-newMember.addEventListener("click", addOneRow);
 
-function addOneRow() {
-    let addOneRow = document.getElementsByClassName("member")[0].innerHTML;
-    addRow(1, addOneRow);
-}
 
-let edit = document.getElementsByClassName("edit")[0];
-
-/*edit.addEventListener("click", function() {})*/
